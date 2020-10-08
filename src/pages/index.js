480df68f51abc4a913c80d45853de1e5c8c82fbb
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-08 18:50:20 +0500

    project bootstrap

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Result } from 'antd'
+
+export default () => {
+  return (
+    <Result
+      status="success"
+      title="Welcome to CryptoFlow landing page"
+      subTitle="Hackathon Project"
+    />
+  )
+}
